3bf40a89f7b0056e2e0532fc086264ac3d633da7
Unknown <daniel.bh38@gmail.com>
2018-10-19 14:36:15 +0300

    beta 1.1

--- script_mtm3.bash
+++ script_mtm3.bash
@@ -1,28 +1,35 @@
-#!bin/bash
+#!/bin/bash
 gcc -std=c99 -o mtm  -Wall -pedantic-errors -Werror -L.  Sources/Student.c Sources/CourseManager.c Sources/Friend.c Sources/FriendRequest.c Sources/Grade.c Tests/student_test.c Sources/Course.c libmtm.a 
-valgrind ./mtm | grep  "All heap blocks were freed -- no leaks are possible"
+valgrind  --leak-check=full  --log-file="tmp.out" ./mtm > tmp.out 2>&1
+grep " All heap blocks were freed -- no leaks are possible" tmp.out
 echo student_test
  gcc -std=c99 -o mtm  -Wall -pedantic-errors -Werror -L.  Sources/Student.c Sources/CourseManager.c Sources/Friend.c Sources/FriendRequest.c Sources/Grade.c Tests/course_test.c Sources/Course.c libmtm.a 
-valgrind ./mtm | grep  "All heap blocks were freed -- no leaks are possible"
+valgrind --leak-check=full --log-file="tmp.out" ./mtm > tmp.out 2>&1
+grep  "All heap blocks were freed -- no leaks are possible" tmp.out
 echo course_test
  
  gcc -std=c99 -o mtm  -Wall -pedantic-errors -Werror -L.  Sources/Student.c Sources/CourseManager.c Sources/Friend.c Sources/FriendRequest.c Sources/Grade.c Tests/coursemanager_test.c Sources/Course.c libmtm.a 
-valgrind ./mtm | grep  "All heap blocks were freed -- no leaks are possible"
+valgrind --leak-check=full --log-file="tmp.out" ./mtm > tmp.out 2>&1
+grep  "All heap blocks were freed -- no leaks are possible" tmp.out
 echo coursemanager_test
  
  gcc -std=c99 -o mtm  -Wall -pedantic-errors -Werror -L.  Sources/Student.c Sources/CourseManager.c Sources/Friend.c Sources/FriendRequest.c Sources/Grade.c Tests/friend_test.c Sources/Course.c libmtm.a 
-valgrind ./mtm | grep  "All heap blocks were freed -- no leaks are possible"
+valgrind --leak-check=full --log-file="tmp.out" ./mtm > tmp.out 2>&1
+grep  "All heap blocks were freed -- no leaks are possible" tmp.out
 echo friend_test
  
  gcc -std=c99 -o mtm  -Wall -pedantic-errors -Werror -L.  Sources/Student.c Sources/CourseManager.c Sources/Friend.c Sources/FriendRequest.c Sources/Grade.c Tests/friendRequest_test.c Sources/Course.c libmtm.a 
-valgrind ./mtm | grep  "All heap blocks were freed -- no leaks are possible"
+valgrind --leak-check=full --log-file="tmp.out" ./mtm > tmp.out 2>&1
+grep  "All heap blocks were freed -- no leaks are possible" tmp.out
 echo friendRequest_test
  
  gcc -std=c99 -o mtm  -Wall -pedantic-errors -Werror -L.  Sources/Student.c Sources/CourseManager.c Sources/Friend.c Sources/FriendRequest.c Sources/Grade.c Tests/grade_test.c Sources/Course.c libmtm.a 
-valgrind ./mtm | grep  "All heap blocks were freed -- no leaks are possible"
+valgrind --leak-check=full --log-file="tmp.out" ./mtm > tmp.out 2>&1
+grep  "All heap blocks were freed -- no leaks are possible" tmp.out
 echo grade_test
  
  gcc -std=c99 -o mtm  -Wall -pedantic-errors -Werror -L.  Sources/Student.c Sources/CourseManager.c Sources/Friend.c Sources/FriendRequest.c Sources/Grade.c Tests/list_mtm_test.c Sources/Course.c libmtm.a 
-valgrind ./mtm | grep  "All heap blocks were freed -- no leaks are possible"
+valgrind --leak-check=full --log-file="tmp.out" ./mtm > tmp.out 2>&1
+grep  "All heap blocks were freed -- no leaks are possible" tmp.out
 echo list_mtm_test
  
